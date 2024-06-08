0e8fc2de5935fb90c270f6e519bdc24dfc54a5ac
Andrius Lukocius <andrius.l@omnisend.com>
2024-06-08 12:55:39 +0300

    rebrand

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -27,7 +27,7 @@ const manifest = Object.assign(
     version: packageJson.version,
     description: '__MSG_extensionDescription__',
     permissions: ['storage'].concat(sidePanelConfig.permissions),
-    options_page: 'options/index.html',
+    // options_page: 'options/index.html',
     background: {
       service_worker: 'background.iife.js',
       type: 'module',
@@ -36,9 +36,9 @@ const manifest = Object.assign(
       default_popup: 'popup/index.html',
       default_icon: 'icon-34.png',
     },
-    chrome_url_overrides: {
-      newtab: 'newtab/index.html',
-    },
+    // chrome_url_overrides: {
+    //   newtab: 'newtab/index.html',
+    // },
     icons: {
       128: 'icon-128.png',
     },
@@ -56,7 +56,7 @@ const manifest = Object.assign(
         css: ['content.css'], // public folder
       },
     ],
-    devtools_page: 'devtools/index.html',
+    // devtools_page: 'devtools/index.html',
     web_accessible_resources: [
       {
         resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
